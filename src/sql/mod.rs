a370ae01b6c083bf60954430c9873cd3df1e4e37
bunburya <dev@bunburya.eu>
2025-05-26 23:48:35 +0100

    fixing tests

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,22 +1,27 @@
 mod from_row;
 
-use sqlx::{AnyPool, Executor, SqlitePool};
+use sqlx::{Executor, SqlitePool};
 
 const SQL_SCHEMA: &str = include_str!("../../sql/schema.sql");
 
-pub async fn init_db(pool: &SqlitePool) -> Result<(), ()> {
-    pool.execute_many(SQL_SCHEMA);
+pub async fn init_db(pool: &SqlitePool) -> Result<(), Box<dyn std::error::Error>> {
+    pool.execute(SQL_SCHEMA).await?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use crate::sql::init_db;
-use sqlx::{Sqlite, SqlitePool, migrate::MigrateDatabase};
+    use sqlx::{Sqlite, sqlite::SqlitePoolOptions, migrate::MigrateDatabase};
+    
+    const SQLITE_URL : &str = "sqlite://test_output/test.db";
+    
     #[tokio::test]
     async fn test_init_db() {
-        Sqlite::create_database("sqlite://test.db").await.unwrap();
-        let pool = SqlitePool::connect("sqlite://test.db").await.unwrap();
+        Sqlite::create_database(SQLITE_URL).await.unwrap();
+        let pool = SqlitePoolOptions::new()
+            .connect(SQLITE_URL)
+            .await.unwrap();
         init_db(&pool).await.unwrap();
     }
 }
